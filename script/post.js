11c6541e395411f90e5d4b2a6a382ed247a29dd0
sunankang <3525166560@qq.com>
2025-03-03 11:23:50 +0800

    3

---
+++ script/post.js
@@ -0,0 +1,22 @@
+var address = window.location.href
+let addressSplit = address.split("/")
+let postNum = addressSplit[addressSplit.length - 1]
+let havePrev = true;
+if (postNum == "1") {
+    havePrev = false;
+    document.getElementById("prev").style.display = "none"
+}
+
+
+function prev() {
+    if (havePrev) {
+        addressSplit[addressSplit.length - 1] = parseInt(postNum) - 1;
+        window.location.href = `${address}`;
+    }
+}
+
+function next() {
+    addressSplit[addressSplit.length - 1] = parseInt(postNum) + 1;
+    window.location.href = `${address}`;
+}
+
