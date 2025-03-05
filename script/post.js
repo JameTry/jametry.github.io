9e182918fc574bbd5dac8ae034fc120c3db51958
sunankang <3525166560@qq.com>
2025-03-05 10:37:00 +0800

    3

--- script/post.js
+++ script/post.js
@@ -1,22 +1,15 @@
 var address = window.location.href
 let addressSplit = address.split("/")
 let postNum = addressSplit[addressSplit.length - 1]
-let havePrev = true;
+
+
 if (postNum == "1") {
-    havePrev = false;
     document.getElementById("prev").style.display = "none"
 }
 
+addressSplit[addressSplit.length - 1] = parseInt(postNum) - 1
+document.getElementById("prev").href = `${addressSplit.join("/")}`;
+addressSplit[addressSplit.length - 1] = parseInt(postNum) + 2;
+document.getElementById("next").href = `${addressSplit.join("/")}`;
 
-function prev() {
-    if (havePrev) {
-        addressSplit[addressSplit.length - 1] = parseInt(postNum) - 1;
-        window.location.href = `${addressSplit.join("/")}`;
-    }
-}
-
-function next() {
-    addressSplit[addressSplit.length - 1] = parseInt(postNum) + 1;
-    window.location.href = `${addressSplit.join("/")}`;
-}
 
