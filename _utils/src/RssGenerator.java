8d095d09ae9260e35d74103c07d15da5ec000d30
sunankang <3525166560@qq.com>
2025-04-15 14:59:07 +0800

    3

--- _utils/src/RssGenerator.java
+++ _utils/src/RssGenerator.java
@@ -21,7 +21,9 @@ public class RssGenerator {
     // 日期格式
     private static final SimpleDateFormat INPUT_DATE = new SimpleDateFormat("yyyy年MM月dd日");
     private static final SimpleDateFormat OUTPUT_DATE = new SimpleDateFormat("dd MMM yyyy HH:mm:ss z", Locale.ENGLISH);
-
+    static {
+        OUTPUT_DATE.setTimeZone(TimeZone.getTimeZone("GMT")); // 强制GMT时区
+    }
     // 定义类型常量
     private static final String TYPE_ARTICLE = "随笔";
     private static final String TYPE_STATUS = "说说";
