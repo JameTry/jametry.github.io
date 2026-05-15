5228d68e83017dc023f5d59080400d3a749db197
Jame <3525166560@qq.com>
2026-05-15 23:15:57 +0800

    3

--- js/tool.js
+++ js/tool.js
@@ -1,3 +1,19 @@
+(function () {
+    const storedTheme = localStorage.getItem('theme');
+    document.documentElement.setAttribute('data-theme',
+        storedTheme || 'light'
+    );
+})();
+
+function toggleTheme() {
+    const currentTheme = document.documentElement.getAttribute('data-theme');
+    const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+    document.documentElement.setAttribute('data-theme', newTheme);
+    localStorage.setItem('theme', newTheme);
+}
+
+
+
 document.addEventListener("DOMContentLoaded", function () {
     //图片预览====
     const viewer = document.createElement('div');
